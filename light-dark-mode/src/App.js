25eef531cad3bb082ba56aefdb66c2a8015b1eba
Divine Higgins <dhiggins94@gmail.com>
2022-06-01 20:48:56 -0400

    dark-light functionality is tied to a switch now

--- light-dark-mode/src/App.js
+++ light-dark-mode/src/App.js
@@ -1,11 +1,37 @@
-import './App.css';
-import Form from './Form';
+import "./App.css";
+import Form from "./Form";
+import { createContext, useState } from "react";
+import Switch from "react-switch";
+
+export const ThemeContext = createContext(null);
+
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggle = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+
   return (
-    <div className="App" id="light">
-  <Form />
-    </div>
+    <ThemeContext.Provider value={{ theme, toggle }}>
+      <div className="App" id={theme}>
+        <Form />
+        <div className="switch">
+          <Switch onChange={toggle} checked={theme === "dark"}/>
+        </div>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
 export default App;
+//  context API allows us to pass toggling functionality as well as determing the mode where in have it accessable anywhere in the app
+
+// from react website
+// Context provides a way to pass data through the component tree without having to pass props down manually at every level.
+
+// the toggle function allows the current value to be checked for being either light or dark which is used for the THEMECONTEXT.PROVIDER for the context. once it figures out the curr(rent) value, it becomes said light or dark
+
+// added react switch libraray for the switch. 
+// (property) checked: boolean
+// The checked state of the switch. If true, the switch is set to checked. If false, it is not checked.
--- light-dark-mode/src/Form.js
+++ light-dark-mode/src/Form.js
@@ -1,35 +1,23 @@
-import React from "react";
+import React from 'react'
 
-export default function Form() {
+export default function 
+() {
   return (
-    <div>
-      <div className="main">
-        <p className="sign" align="center">
-          Sign in
-        </p>
-        <form className="form1">
-          <input
-            className="un "
-            type="text"
-            align="center"
-            placeholder="Username"
-          />
-          <input
-            className="pass"
-            type="password"
-            align="center"
-            placeholder="Password"
-          />
-          <a className="submit" align="center">
-            Sign in
-          </a>
-          <p className="forgot" align="center">
-            <a href="#">Forgot Password?</a>
-          </p>
-          <a href="#"></a>
-        </form>
-      </div>
-      <a href="#"></a>
-    </div>
-  );
+    <div className="main">
+    <p className="sign" align="center">
+      Sign in
+    </p>
+    <form className="form1">
+      <input className="username" type="text" placeholder="Username" />
+      <input className="password" type="password" placeholder="Password" />
+      <a className="submit" align="center">
+        Sign in
+      </a>
+      <p className="forgot" align="center">
+        <a href="#">Forgot Password? </a>
+      </p>
+    </form>
+  </div>
+      
+  )
 }
